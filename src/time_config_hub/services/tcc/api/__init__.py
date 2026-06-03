3f18c86d339808e72b9555ce807da566ef644c52
ishamisu <hea.ming.su@intel.com>
2026-06-03 22:42:49 +0800

    feat(TccDataModel): Add data types and a data modelling API to make accessing YANG XML data easier

---
+++ src/time_config_hub/services/tcc/api/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2026 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+
+# TCCDataAPI: primary interface for querying parsed TCC configuration data
+
+from time_config_hub.services.tcc.api.data_api import TCCDataAPI
+
+__all__ = ["TCCDataAPI"]
